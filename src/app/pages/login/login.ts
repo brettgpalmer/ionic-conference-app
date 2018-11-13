865e17fa96af568f7d45bae2a5f2e7594fdb6bb5
bgpalmer <brettgpalmer@gmail.com>
2018-11-13 08:24:21 -0700

    Added initial speakers service for loading to FB

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,7 +1,9 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { LoadingController, AlertController } from '@ionic/angular';
 import { AuthService } from '../../providers/auth.service';
+import { SpeakersService } from '../../providers/speakers.service';
 import { UserData } from '../../providers/user-data';
 
 import { UserOptions } from '../../interfaces/user-options';
@@ -15,13 +17,19 @@ import { UserOptions } from '../../interfaces/user-options';
   encapsulation: ViewEncapsulation.None
 })
 export class LoginPage {
+
+  public loading: HTMLIonLoadingElement;
+
   login: UserOptions = { username: '', password: '' };
   submitted = false;
 
   constructor(
     public userData: UserData,
     public router: Router,
-    public authService: AuthService
+    public authService: AuthService,
+    public speakersService: SpeakersService,
+    public loadingCtrl: LoadingController,
+    public alertCtrl: AlertController,
   ) { }
 
   onLogin(form: NgForm) {
@@ -30,6 +38,39 @@ export class LoginPage {
     if (form.valid) {
       this.userData.login(this.login.username);
       this.router.navigateByUrl('/app/tabs/(schedule:schedule)');
+    }
+  }
+
+  importSpeakers() {
+
+    this.loading = await this.loadingCtrl.create();
+
+    this.speakersService.importSpeakers();
+  }
+
+  importOneSpeaker() {
+    try {
+      const name = 'David Lee Roth';
+      const profilePic = 'assets/img/speakers/bear.jpg';
+      const twitter = 'ionicframework';
+      const about = 'Rockstar';
+      const location = 'Los Angeles, CA';
+      const email = 'droth@vanhelen.org';
+      const phone = '213-222-4444';
+
+      this.speakersService.addSpeaker(
+        name,
+        profilePic,
+        twitter,
+        about,
+        location,
+        email,
+        phone
+      ).then(() => {
+          this.router.navigateByUrl('/survey-list');
+      });
+    } catch (error) {
+        console.error(error);
     }
   }
 
---
+++ src/app/providers/speakers.service.ts
@@ -0,0 +1,105 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+
+import { AngularFireAuth } from '@angular/fire/auth';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+  AngularFirestoreDocument,
+} from '@angular/fire/firestore';
+import {
+  AngularFireStorage,
+  AngularFireStorageReference } from '@angular/fire/storage';
+import * as firebase from 'firebase/app';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpeakersService {
+
+  public speakerList: AngularFirestoreCollection<any>;
+  public userId: string;
+
+  constructor(
+    public http: HttpClient,
+    private afAuth: AngularFireAuth,
+    private firestore: AngularFirestore,
+    private afStorage: AngularFireStorage
+  ) {
+
+    this.afAuth.authState.subscribe(user => {
+      this.userId = user.uid;
+      this.speakerList = this.firestore.collection(`/speakerList`);
+    });
+  }
+
+  getSpeakerList(): AngularFirestoreCollection<any> {
+    return this.speakerList;
+  }
+
+  async addSpeaker(
+    name: string,
+    profilePic: string,
+    twitter: string,
+    about: string,
+    location: string,
+    email: string,
+    phone: string,
+  ): Promise<any> {
+    const newSpeakerRef: firebase.firestore.DocumentReference = await this.speakerList.add({});
+
+    return newSpeakerRef.update({
+      name,
+      profilePic,
+      twitter,
+      about,
+      location,
+      email,
+      phone,
+      id: newSpeakerRef.id,
+    });
+  }
+
+  /* Run through list of speaker data and add to FB */
+
+  importSpeakers(): Promise<any> {
+
+    return this.http
+      .get('assets/data/speaker_data.json')
+      .pipe(map(this.processData, this));
+  }
+
+  processData(data: any) {
+
+    data.speakers.forEach(( speaker: any) => {
+
+      try {
+        this.addSpeaker(
+          speaker.name,
+          speaker.profilePic,
+          speaker.twitter,
+          speaker.about,
+          speaker.location,
+          speaker.email,
+          speaker.phone)
+          .then(() => {
+            console.log( data );
+          });
+      } catch (error) {
+        console.error(error);
+      }
+    });
+  }
+/*
+  "name": "Burt Bear",
+  "profilePic": "assets/img/speakers/bear.jpg",
+  "twitter": "ionicframework",
+  "about": "Burt is a Bear.",
+  "location": "Everywhere",
+  "email": "burt@example.com",
+  "phone": "+1-541-754-3010",
+  "id": "1"
+*/
+
+}
